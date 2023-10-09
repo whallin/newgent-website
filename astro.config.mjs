7968e242c315e06e142e43a0657bae830279b096
William Hallin <william.hallin@newgent.digital>
2023-10-09 02:23:04 +0200

    feat: :heavy_plus_sign: optimize via jampack

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import prefetch from "@astrojs/prefetch";
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), prefetch(), partytown()],
+  integrations: [tailwind(), sitemap(), partytown()],
   site: "https://newgent.digital",
 });
---
+++ jampack.config.mjs
@@ -0,0 +1,13 @@
+export default {
+  image: {
+    external: {
+      process: "download",
+    },
+  },
+  css: {
+    inline_critical_css: true,
+  },
+  misc: {
+    prefetch_links: "in-viewport",
+  },
+};
