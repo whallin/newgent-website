0227cfa6e783fd02e5403764a7b63c1029b8af6a
William Hallin <william.hallin@newgent.digital>
2023-10-09 02:08:59 +0200

    feat: :tada: the real beginning
    replaces bun & sets up prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), sitemap(), prefetch(), partytown()],
+  site: "https://newgent.digital",
+});
