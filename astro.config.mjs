59d3f24192b10c8c51346f516199ba6824013693
William Hallin <william.hallin@newgent.digital>
2023-10-29 17:53:59 +0100

    feat: proper i18n sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,12 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), partytown()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      i18n: { defaultLocale: "en", locales: { en: "en-US", sv: "sv-SE" } },
+    }),
+    partytown(),
+  ],
   site: "https://newgent.digital",
 });
