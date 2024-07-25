24bbbeeafaf31d23cf8114413624479c02496a53
William Hallin <william@hallin.media>
2024-07-26 01:57:58 +0200

    feat: prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://newgent.digital',
+	prefetch: true,
 	integrations: [
 		tailwind(),
 		playformCompress(),
