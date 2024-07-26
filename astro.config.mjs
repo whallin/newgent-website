797ca7f9eb1b7c822ac587253550efa2312dca8a
William Hallin <william@hallin.media>
2024-07-26 13:32:38 +0200

    refactor: minify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import playformCompress from '@playform/compress'
-import icon from 'astro-icon'
 import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://newgent.digital',
 	prefetch: true,
-	integrations: [
-		tailwind(),
-		playformCompress(),
-		icon({
-			include: {
-				tabler: ['*']
-			}
-		}),
-		sitemap()
-	]
+	integrations: [tailwind(), playformCompress(), sitemap()]
 })
