ac5af90c2f4489eadbf202fac06725b460b2453e
William Hallin <william@hallin.media>
2024-07-27 12:27:20 +0200

    feat: partytown & analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,20 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import playformCompress from '@playform/compress'
 import sitemap from '@astrojs/sitemap'
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://newgent.digital',
 	prefetch: true,
-	integrations: [tailwind(), playformCompress(), sitemap()]
+	integrations: [
+		tailwind(),
+		playformCompress(),
+		sitemap(),
+		partytown({
+			config: {
+				forward: ['dataLayer.push']
+			}
+		})
+	]
 })
